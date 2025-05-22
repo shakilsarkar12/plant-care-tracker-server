e5f67b306c7185263de976cdf8d3fabc96198dca
shakil <shakil.sarkar1932@gmail.com>
2025-05-22 21:45:37 +0600

    plantdelete API create

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/plantdelate/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await plantsCollection.deleteOne(query);
+      res.send(result);
+    })
+
   } finally {
   }
 }
